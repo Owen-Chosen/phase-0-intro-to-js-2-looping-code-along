b438e07bca8ec34650e1a211c19bbcbf5f7171c0
owen <owen.chosen@gmail.com>
2023-07-21 00:53:47 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards (arr = [], myEvent) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArray.push (`Thank you, ${arr[i]}, for the wonderful ${myEvent} gift!`);
+    }
+    return newArray;
+}
+
+function countDown (num) {
+    while (num >= 0) {
+        console.log (num);
+        num--;
+    }
+}
+
